122b4396ce7bcca8ee4addcee53d33b99e0aaf73
touros91 <andreageraci91@gmail.com>
2021-10-19 14:32:18 +0200

    Refactoring: aggiunto codice PHP nell'HTML

--- Snack-1/index.php
+++ Snack-1/index.php
@@ -5,55 +5,75 @@ Olimpia Milano - Cantù | 55-60 -->
 <?php 
     $partite = [
         [
-            "squadraCasa" => "Germani Brescia",
-            "squadraOspite" => "Virtus Segafredo Bologna",
+            "squadraCasa" => "Brescia",
+            "squadraOspite" => "Virtus Bologna",
             "puntiSquadraCasa" => 65,
             "puntiSquadraOspite" => 70
         ],
         [
-            "squadraCasa" => "Allianz Pallacanestro Trieste",
-            "squadraOspite" => "Gevi Napoli",
+            "squadraCasa" => "Trieste",
+            "squadraOspite" => "Basket Napoli",
             "puntiSquadraCasa" => 49,
             "puntiSquadraOspite" => 57
         ],
         [
-            "squadraCasa" => "A|X Armani Exchange Milano",
-            "squadraOspite" => "Happy Casa Brindisi",
+            "squadraCasa" => "Olimpia Milano",
+            "squadraOspite" => "Brindisi",
             "puntiSquadraCasa" => 55,
             "puntiSquadraOspite" => 60
         ],
         [
-            "squadraCasa" => "Banco di Sardegna Sassari",
-            "squadraOspite" => "Nutribullet Treviso Basket",
+            "squadraCasa" => "Sassari",
+            "squadraOspite" => "Treviso",
             "puntiSquadraCasa" => 74,
             "puntiSquadraOspite" => 45
         ],
         [
-            "squadraCasa" => "Bertram Derthona Tortona",
-            "squadraOspite" => "Openjobmetis Varese",
+            "squadraCasa" => "Tortona",
+            "squadraOspite" => "Varese",
             "puntiSquadraCasa" => 38,
             "puntiSquadraOspite" => 62
         ],
         [
-            "squadraCasa" => "Carpegna Prosciutto Pesaro",
-            "squadraOspite" => "Umana Reyer Venezia",
+            "squadraCasa" => "Pesaro",
+            "squadraOspite" => "Cremona",
             "puntiSquadraCasa" => 52,
             "puntiSquadraOspite" => 62
         ],
         [
-            "squadraCasa" => "Dolomiti Energia Trentino",
-            "squadraOspite" => "UNAHOTELS Reggio Emilia",
+            "squadraCasa" => "Trento",
+            "squadraOspite" => "Reggiana",
             "puntiSquadraCasa" => 54,
             "puntiSquadraOspite" => 66
         ],
         [
             "squadraCasa" => "Fortitudo Bologna",
-            "squadraOspite" => "Vanoli Basket Cremona",
+            "squadraOspite" => "Venezia",
             "puntiSquadraCasa" => 79,
             "puntiSquadraOspite" => 57
         ]
     ];
-    for ($i = 0; $i < count($partite); $i++) {
-        echo $partite[$i]["squadraCasa"] . " - " . $partite[$i]["squadraOspite"] . " | " . $partite[$i]["puntiSquadraCasa"] . " - " . $partite[$i]["puntiSquadraOspite"] . "<br>";
-    }
+    // for ($i = 0; $i < count($partite); $i++) {
+    //     echo $partite[$i]["squadraCasa"] . " - " . $partite[$i]["squadraOspite"] . " | " . $partite[$i]["puntiSquadraCasa"] . " - " . $partite[$i]["puntiSquadraOspite"] . "<br>";
+    // }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lega Basket Serie A</title>
+</head>
+<body>
+    <h1>Risultati Basket Serie A</h1>
+    <p>
+       <?php 
+            for ($i = 0; $i < count($partite); $i++) {
+                echo $partite[$i]["squadraCasa"] . " - " . $partite[$i]["squadraOspite"] . " | " . $partite[$i]["puntiSquadraCasa"] . " - " . $partite[$i]["puntiSquadraOspite"] . "<br>";
+            }
+       ?>
+    </p> 
+</body>
+</html>
